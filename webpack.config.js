1e930f1aa36f2c3b0add5fe897e1d4c97be38571
Ben Windsor <bjw2@uk.cambridgeconsultants.com>
2017-07-03 10:52:41 +0100

    Initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    // Set the "homepage"
+    entry: "./src/index.tsx",
+    
+    // Output the js bundle to the dist folder
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        ]
+    },
+
+     plugins: [
+        new CopyWebpackPlugin([
+            { from: './node_modules/react/dist/react.js' },
+            { from: './node_modules/react-dom/dist/react-dom.js' },
+            { from: 'node_modules/leaflet/dist/leaflet.css'},
+            { from: './src/style.css'}
+        ])
+     ],
+
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+};
