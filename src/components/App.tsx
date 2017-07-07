0c6f2d4097d1b85fbeb32a47b5751da5bddc86cf
Ben Windsor <benwindsor@gmail.com>
2017-07-08 00:15:39 +0100

    Switching between viewing trainings now works

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -44,18 +44,27 @@ export class App extends React.Component<undefined, AppState> {
             }).catch(err=>console.log(err))
         })
     }
+    onNavClick(e: React.MouseEvent<HTMLTableRowElement>) : void {
+        let m = e.currentTarget.id.match(/\d+/)
+        if (m!=null && !Number.isNaN(Number.parseInt(m[0]))) {
+            this.fetchTraining(Number.parseInt(m[0]));
+        } else {
+            console.log('Error during click callback')
+        }
+    }
 
     render() {
+        let currentId = (this.state.training==null)?null:this.state.training.id;
         return (
             <div>
                 
                  <div className={'training-list'}>
-                    <h1>Trainings</h1>
-                    <table>
-                    {this.state.allTrainings.map(t => {
+                    <h1>Training Editor</h1>
+                    <table className="nav-table">
+                    {this.state.allTrainings.map((t, i) => {
                         let d = new Date(t.date_start*1000)
                         return (
-                        <tr>
+                        <tr className={(currentId!=null && currentId==t.id)?'navRow-selected':'navRow'} id={'nav'+t.id.toString()} key={t.id.toString()} onClick={e=>this.onNavClick(e)}>
                             <td>{d.getDate()}/{d.getMonth()}</td><td>{t.location_name}</td>
                         </tr>
                     )})}
